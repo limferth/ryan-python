c535cd61890bdf819987a99ca4bfb7b9cb650766
limferth <limferth@gmail.com>
2024-08-25 18:01:44 -0400

    variables commit

---
+++ variables.py
@@ -0,0 +1,18 @@
+x = 5
+y = 7
+
+print(x+y)
+
+# como usar las variablres para hacking
+
+fname = input("what is your name")
+lname = input("what is your last name")
+
+print(f"your name is: {fname} {lname}")
+
+
+# excersisise
+
+pname = input("whats is the name of a pet? ")
+bcity = input("whtas city were you born in? ")
+print(f"your new x handle and blo: @Ciber{pname} from {bcity}")
