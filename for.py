e695e40281ebd8676a2148085999c12d70a3209a
limferth <limferth@gmail.com>
2024-08-25 18:01:24 -0400

    for commit

---
+++ for.py
@@ -0,0 +1,21 @@
+# fruit = ["apple", "banana", "cherry"]
+
+# for i in fruits:
+#     print(i + "pie")
+
+
+for num in range (1, 100):
+    if num % 3 == 0:
+        print(num)
+
+# exceises
+
+for num in range(1, 100):
+    if num % 3 == 0 and num % 5 == 0:
+        print("fizzbuzz")
+    elif num % 3 ==0:
+        print("fizz")
+    elif num % 5 ==0:
+        print("buzz")
+    else:
+        print(num)
